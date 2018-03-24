2d1d9ce0bbbd88fa7faa8cb9d6321aa8a59b2011
Stefan <szdralje@gmail.com>
2018-03-24 17:47:56 -0500

    plot to visualize outliers - to check functions not for end user

---
+++ R/visualize_outliers.R
@@ -0,0 +1,6 @@
+# df_outliers%>%
+#     ggplot()+
+#     aes(x=trait, y = phenotype)+
+#     geom_boxplot(outlier.color = NA)+
+#     geom_jitter(aes(color = outlier))+
+#     facet_wrap(~trait, scales= "free")
