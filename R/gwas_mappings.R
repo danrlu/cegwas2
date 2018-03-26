03ec222cbca7375294017b74ddfee00e82b60828
Stefan <szdralje@gmail.com>
2018-03-26 18:58:10 -0500

    added qvalue to perform_mapping output, function stolen from rrblup

--- R/gwas_mappings.R
+++ R/gwas_mappings.R
@@ -68,18 +68,71 @@ perform_mapping <- function(phenotype = NULL,
                                n.core = parallel::detectCores(),
                                P3D = P3D)
 
+    yfit <- rrBLUP::mixed.solve(y = Y$trait, K = K)
+    kfit <- rrBLUP::kin.blup(data = data.frame(Y), geno = "strain", pheno = "trait", K = K)
+
     # Process mapping results
     gwa_results_pr <- gwa_results %>%
         dplyr::rename(log10p = trait) %>%
         dplyr::filter(log10p != 0) %>%
-        dplyr::mutate(BF = log10(.05/n()),
+        dplyr::mutate(BF = log10(.05/n()), # set bonferroni threshold
                       trait = colnames(Y)[2]) %>%
         dplyr::select(CHROM, POS, marker, trait, BF, log10p) %>%
-        dplyr::mutate(pval = 10^-log10p) %>%
-        dplyr::mutate(Zscore = (pval - mean(pval)) / sd(pval))
+        dplyr::mutate(pval = 10^-log10p) %>% # convert log10p to p
+        dplyr::mutate(Zscore = (pval - mean(pval)) / sd(pval), # calculate z-score from pvalue distribution
+                      qvalue = qvalue(pval)) # calculate q-value from pvalue distribution
 
     return(gwa_results_pr)
 }
 
+# Taken from rrBLUP GWAS function ~ https://github.com/cran/rrBLUP/blob/master/R/GWAS.R
+qvalue <- function(p) {
+    smooth.df = 3
 
+    if(min(p)<0 || max(p)>1) {
+        print("ERROR: p-values not in valid range.")
+        return(0)
+    }
 
+    lambda=seq(0,0.90,0.05)
+    m <- length(p)
+
+    pi0 <- rep(0,length(lambda))
+    for(i in 1:length(lambda)) {pi0[i] <- mean(p >= lambda[i])/(1-lambda[i])}
+
+    spi0 <- smooth.spline(lambda,pi0,df=smooth.df)
+    pi0 <- predict(spi0,x=max(lambda))$y
+    pi0 <- min(pi0,1)
+
+    if(pi0 <= 0) {
+        print("ERROR: The estimated pi0 <= 0. Check that you have valid p-values.")
+        return(0)
+    }
+
+    #The estimated q-values calculated here
+    u <- order(p)
+
+    # ranking function which returns number of observations less than or equal
+    qvalue.rank <- function(x) {
+        idx <- sort.list(x)
+
+        fc <- factor(x)
+        nl <- length(levels(fc))
+        bin <- as.integer(fc)
+        tbl <- tabulate(bin)
+        cs <- cumsum(tbl)
+
+        tbl <- rep(cs, tbl)
+        tbl[idx] <- tbl
+
+        return(tbl)
+    }
+
+    v <- qvalue.rank(p)
+
+    qvalue <- pi0*m*p/v
+    qvalue[u[m]] <- min(qvalue[u[m]],1)
+    for(i in (m-1):1) {qvalue[u[i]] <- min(qvalue[u[i]],qvalue[u[i+1]],1)}
+
+    return(qvalue)
+}
