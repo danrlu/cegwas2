aae5f40862fe1cfb005a81eea9fc885cee8f70e4
Stefan <szdralje@gmail.com>
2018-03-27 20:29:23 -0500

    clean up gwas_mappigns for merge

--- R/visualize_outliers.R
+++
@@ -1,6 +0,0 @@
-# df_outliers%>%
-#     ggplot()+
-#     aes(x=trait, y = phenotype)+
-#     geom_boxplot(outlier.color = NA)+
-#     geom_jitter(aes(color = outlier))+
-#     facet_wrap(~trait, scales= "free")
